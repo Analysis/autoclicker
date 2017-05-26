506589767f3ce41159fdc1817f5bcf5b48ba58e1
levondov <levondov@berkeley.edu>
2017-05-26 13:56:43 -0700

    updated thieving script

---
+++ thieving_script.py
@@ -0,0 +1,40 @@
+import subprocess
+import time
+import random
+import sys
+
+try:
+	alch_arg = sys.argv[1]
+except:
+	alch_arg = 2**16
+
+
+def alching(alch=alch_arg):
+
+	random.seed() # initialize random seed generator - use current system time as the hashable object
+	for i in range(0,int(alch)):
+		print "Click: " + str(i) + " / " + str(alch)
+	# move mouse over the correct point on the screen
+	# use the 'xdotool getmouselocation' command in terminal to get coordinates for current location
+	#mouse_X = 784
+	#mouse_Y = 294
+	#subprocess.call(["xdotool", "mousemove", str(mouse_X), str(mouse_Y)])
+
+		# press down on the mouse
+		subprocess.call(["xdotool", "mousedown", "1"])
+	
+		# sleep for a random interval between 0.1 - 0.5 seconds
+		upper_range = random.randint(350,500)
+		lower_range = random.randint(150,300)
+		time.sleep(random.randint(lower_range,upper_range) / 1000.0)
+		
+		# release mouse click
+		subprocess.call(["xdotool", "mouseup", "1"])
+		
+		# random delay before next click
+		upper_range = random.randint(300,400)
+		lower_range = random.randint(50,200)
+		time.sleep(random.randint(lower_range,upper_range) / 1000.0)
+
+if __name__ == "__main__":
+	alching()
